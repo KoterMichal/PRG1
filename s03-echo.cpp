ea41a9f09765423aa3d7251f31e168dc3feb29ab
Koter <s24569@pjwstk.edu.pl>
2021-10-15 16:51:37 +0200

    s03-echo

---
+++ s03-echo.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+
+auto main(int argc, char* argv[]) -> int
+{
+    if(argv[1][0] == '-'){
+
+        if(argv[2][0] == '-'){
+            if((argv[1][1] == 'r' && argv[2][1] == 'l') || (argv[1][1] == 'l' && argv[2][1] == 'r')){
+                for(int i = argc - 1; i > 0; i--){
+                    std::cout << argv[i] << "\n";
+                }
+            }
+            else if((argv[1][1] == 'r' && argv[2][1] == 'n') || (argv[1][1] == 'n' && argv[2][1] == 'r')){
+                for(int i = argc - 1; i > 0; i--){
+                    std::cout << argv[i] << " ";
+                }
+            }
+        }
+
+        if(argv[1][1] == 'n'){
+            for(int i=1; i < argc; i++){
+                std::cout << argv[i] << " ";
+            }
+        } else if(argv[1][1] == 'r'){
+            for(int i=argc - 1; i > 0; i--){
+                std::cout << argv[i] << " ";
+            }
+            std::cout << "\n";
+        } else if(argv[1][1] == 'l'){
+            for(int i=1; i < argc; i++){
+                std::cout << argv[i] << "\n";
+            }
+        }
+    }
+
+    return 0;
+}
