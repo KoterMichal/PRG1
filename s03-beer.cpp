d0a9964440f82e980c4a4b090b3eefb4ca0fc2f8
KoterMichal <s24569@pjwstk.edu.pl>
2021-10-16 12:34:46 +0200

    s03-beer corrected

--- s03-beer.cpp
+++ s03-beer.cpp
@@ -4,16 +4,29 @@ auto main(int argc, char* argv[]) -> int
 {
     auto const buttles = std::stoi(argv[1]);
 
-    for(int i = buttles; i > 1; i--){
-        std::cout << i << " bottles of beer on the wall, "
-                  << i << " bottles of beer. \n"
-                  << "Take one down, pass it around, ";
+    for(int i = buttles; i > 0; i--){
+        if(i > 2){
+            std::cout << i << " bottles of beer on the wall, "
+                      << i << " bottles of beer. \n"
+                      << "Take one down, pass it around, "
+                      << i-1 << " bottles of beer on the wall. \n \n";
+        } else if(i == 2){
+            std::cout << i << " bottles of beer on the wall, "
+                      << i << " bottles of beer. \n"
+                      << "Take one down, pass it around, "
+                      << i-1 << " bottle of beer on the wall. \n \n";
+        } else{
+            std::cout << i << " battle of beer on the wall, "
+                      << i << " bottle of beer. \n"
+                      << "Take one down, pass it around, "
+                      << "No more buttles of beer on the wall \n\n";
+        }
     }
-    std::cout << "1 battles of beer on the wall, 1 bottle of beer. \n"
-              << "Take one down, pass it around, 0 bottle of beer on th wall \n"
-              << "No more buttles of beer on the wall, no more bottles of beer. \n"
-              << "Go to the store and buy some more, "
-              << buttles << " bottles of beer on the wall... \n";
+
+    std::cout << "No more buttles of beer on the wall, "
+              << "no more bottles of beer. \n"
+              << "Got to the store and buy some more, "
+              << buttles << " botteles of beer on the wall... \n";
 
     return 0;
 }
