74391fb892c191391bc10d8e123f3525e539c967
s24569 <s24569@pjwstk.edu.pl>
2021-11-05 15:03:03 +0100

    calculator- add limit of arguments

--- 03-loops/s04-rpn-calculator-v2.cpp
+++ 03-loops/s04-rpn-calculator-v2.cpp
@@ -4,37 +4,39 @@
 
 auto main(int argc,char* argv[]) -> int
 {
-    double x = 0;
-    double y = 0;
-    std::string input;
-    for(int i = 1; i < argc; i++)
-    {
-        input = std::string(argv[i]);
-        if(input == "+"){
-            y = x + y;
-        } else if (input == "-"){
-            y = x - y;
-        } else if (input == "*"){
-            y = x * y;
-        } else if (input == "**"){
-            y= std::pow(x, y);
-        } else if (input == "/"){
-            y = x / y;
-        } else if (input == "/""/"){
-            y = std::floor(x) / std::floor(y);
-        } else if (input == "%"){
-            y = std::fmod(x, y);
-        } else if (input == "sqrt"){
-            y = std::sqrt(y);
-        } else if (input == "hypot") {
-            y = std::hypot(x, y);
-        } else {
-            x = y;
-            y = std::stof(argv[i]);
+    if (argc < 5){
+        double x = 0;
+        double y = 0;
+        std::string input;
+        for(int i = 1; i < argc; i++)
+        {
+            input = std::string(argv[i]);
+            if(input == "+"){
+                y = x + y;
+            } else if (input == "-"){
+                y = x - y;
+            } else if (input == "*"){
+                y = x * y;
+            } else if (input == "**"){
+                y= std::pow(x, y);
+            } else if (input == "/"){
+                y = x / y;
+            } else if (input == "/""/"){
+                y = std::floor(x) / std::floor(y);
+            } else if (input == "%"){
+                y = std::fmod(x, y);
+            } else if (input == "sqrt"){
+                y = std::sqrt(y);
+            } else if (input == "hypot") {
+                y = std::hypot(x, y);
+            } else {
+                x = y;
+                y = std::stof(argv[i]);
+            }
         }
-
-
+        std::cout << y << "\n";
+    } else {
+        std::cout << "ERROR: to many arguments\n";
     }
-    std::cout << y << "\n";
     return 0;
 }
