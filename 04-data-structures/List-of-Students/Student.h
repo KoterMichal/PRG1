d2ee95bbb3411637b20557afc89f3b42b5b4439c
s24569 <91740818+KoterMichal@users.noreply.github.com>
2021-11-24 19:28:34 +0100

    Update Student.h

--- 04-data-structures/List-of-Students/Student.h
+++ 04-data-structures/List-of-Students/Student.h
@@ -4,8 +4,6 @@
 #include <sstream>
 #include <string>
 
-namespace Student {
-
 struct Student{
     std::string const firstname;
     std::string forname;
@@ -14,10 +12,18 @@ struct Student{
     float average_grade;
 
     Student() = default;
-    Student(std::string const, std::string, std::string const, unsigned short, float);
-
+    Student(std::string const firstn, std::string forn, std::string const ib, unsigned short t, float ag)
+        : firstname{std::move(firstn)}
+        , forname{std::move(forn)}
+        , index_book{std::move(ib)}
+        , term{t}
+        , average_grade{ag}
+    {}
+    
     auto to_string() const -> std::string;
+        auto out = std::ostringstream{};
+        out << firstname << " " << forname << " (" << index_book << "), term: " << term << " avarage grade: " << average_grade;
+        return out.str();
 };
 
-}
 #endif
